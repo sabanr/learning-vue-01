750c004a9fd3e8863cecada2917f15e417fa6c7e
Roberto Saban <roberto.saban@sweet.com.ar>
2019-01-19 23:52:05 -0300

    .

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
 <template>
   <div id="app">
+    <header>
+      <nav>
+        <ul>
+          <li class="nav-item">
+            <img class="logo" src=".\assets\build-a-bot-logo.png">
+            Build-a-Bot
+          </li>
+        </ul>
+      </nav>
+    </header>
     <main>
-      <home-page msg="Welcome to Your Vue.js App"/>
+      <HomePage/>
     </main>
   </div>
 </template>
@@ -31,5 +41,24 @@ main {
   background-color: white;
   width: 1024px;
   min-height: 300px;
+}
+header {
+  background-color: #999;
+  width: 1084px;
+  margin: 0 auto;
+}
+ul {
+  padding: 3px;
+  display: flex;
+}
+.nav-item {
+  display: inline-block;
+  padding: 5px 10px;
+  font-size: 22px;
+  border-right: 1px solid #bbb;
+}
+.logo {
+  vertical-align: middle;
+  height: 30px;
 }
 </style>
