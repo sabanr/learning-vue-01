fc152917c7c6f7a59fd20768806f502848749e2b
Roberto Saban <roberto.saban@sweet.com.ar>
2019-01-19 23:53:07 -0300

    wtv

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -4,7 +4,7 @@
       <img class="robot" src="..\assets\robot-home.png" aria-hidden="true">
     </div>
     <div class="get-started">
-      <a href>Get started</a> building your first robot
+      <a href>Get started</a> building your first robot,
     </div>
   </div>
 </template>
