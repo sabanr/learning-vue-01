0751675a119e85a3a5a0cbde590548a4f90a4704
Roberto Saban <roberto.saban@sweet.com.ar>
2019-02-18 22:31:44 -0300

    created side bars and child routes

--- src/App.vue
+++ src/App.vue
@@ -15,10 +15,14 @@
         </ul>
       </nav>
     </header>
-    <main>
-      <router-view/>
-      <!-- <RobotBuilder/> -->
-    </main>
+    <div class="container">
+      <aside class="aside">
+        <router-view name="sidebar"/>
+      </aside>
+      <main>
+        <router-view/>
+      </main>
+    </div>
   </div>
 </template>
 
@@ -46,15 +50,14 @@ body {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
 }
 main {
-  margin: 0 auto;
   padding: 30px;
   background-color: lightgrey;
-  width: 1024px;
+  width: 964px;
   min-height: 300px;
 }
 header {
   background-color: #999;
-  width: 1084px;
+  width: 1184px;
   margin: 0 auto;
 }
 ul {
@@ -81,5 +84,16 @@ ul {
 }
 .router-link-active {
   color: white;
+}
+.container {
+  display: flex;
+  margin: 10px auto 0 auto;
+  justify-content: center;
+}
+.aside {
+  padding: 30px;
+  background-color: #aaa;
+  width: 100px;
+  min-height: 300px;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,13 @@ import Router from 'vue-router';
 import HomePage from '../home/HomePage.vue';
 import RobotBuilder from '../build/RobotBuilder.vue';
 import PartInfo from '../parts/PartInfo.vue';
+import BrowseParts from '../parts/BrowseParts.vue';
+import RobotHeads from '../parts/RobotHeads.vue';
+import RobotArms from '../parts/RobotArms.vue';
+import RobotTorsos from '../parts/RobotTorsos.vue';
+import RobotBases from '../parts/RobotBases.vue';
+import SidebarStandard from '../sidebars/SidebarStandard.vue';
+import SidebarBuild from '../sidebars/SidebarBuild.vue';
 
 
 Vue.use(Router);
@@ -12,12 +19,49 @@ export default new Router({
   routes: [{
     path: '/',
     name: 'Home',
-    component: HomePage,
+    components: {
+      default: HomePage,
+      sidebar: SidebarStandard,
+    },
   },
   {
     path: '/build',
     name: 'Build',
-    component: RobotBuilder,
+    components: {
+      default: RobotBuilder,
+      sidebar: SidebarBuild,
+    },
+  },
+  {
+    path: '/parts/browse',
+    name: 'BrowseParts',
+    component: BrowseParts,
+    children: [{
+      path: 'heads',
+      name: 'BrowseHeads',
+      component: RobotHeads,
+    },
+    {
+      path: 'heads',
+      name: 'BrowseHeads',
+      component: RobotHeads,
+    },
+    {
+      path: 'arms',
+      name: 'BrowseArms',
+      component: RobotArms,
+    },
+    {
+      path: 'torsos',
+      name: 'BrowseTorsos',
+      component: RobotTorsos,
+    },
+    {
+      path: 'bases',
+      name: 'BrowseBases',
+      component: RobotBases,
+    },
+    ],
   },
   {
     path: '/parts/:partType/:id',
